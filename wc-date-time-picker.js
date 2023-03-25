40384ac0efb6469a8a06a82fdcae827584828cb0
LendorBrr <96260311+LendorBrr@users.noreply.github.com>
2023-03-25 20:21:02 +0200

    Add files via upload

---
+++ wc-date-time-picker.js
@@ -0,0 +1,47 @@
+jQuery(document).ready(function($) {
+  // Initialize date and time pickers
+  let minDate = parseInt($("#wc-date-picker").data("min-date"), 10);
+  let minTime = parseInt($("#wc-time-picker").data("min-time"), 10);
+  var availableDates = wcDateTimePickerData.available_dates;
+  var availableTimeSlots = wcDateTimePickerData.available_time_slots;
+  
+
+  function isAvailableDate(date) {
+    var dateString = jQuery.datepicker.formatDate('yy-mm-dd', date);
+    return (availableDates.indexOf(dateString) > -1);
+  }
+
+  $("#wc-date-picker").datepicker({
+    minDate: minDate,
+    dateFormat: "yy-mm-dd",
+    beforeShowDay: function(date) {
+      if (availableDates.length > 0) {
+        return [isAvailableDate(date)];
+      }
+      return [true];
+    },
+  });
+
+  $("#wc-time-picker").timepicker({
+    minTime: minTime,
+    timeFormat: "H:i",
+    disableTimeRanges: availableTimeSlots.length > 0 ? [] : [['12am', '11:59pm']],
+  });
+
+  if (availableTimeSlots.length > 0) {
+    $('#wc-time-picker').on('show.timepicker', function() {
+      var instance = $(this).data('timepicker');
+      var date = $('#wc-date-picker').datepicker('getDate');
+
+      if (isAvailableDate(date)) {
+        instance.option('disableTimeRanges', []);
+        availableTimeSlots.forEach(function(timeSlot) {
+          var timeRange = timeSlot.split('-');
+          instance.option('disableTimeRanges').push(timeRange);
+        });
+      } else {
+        instance.option('disableTimeRanges', [['12am', '11:59pm']]);
+      }
+    });
+  }
+});
---
+++ wc-date-time-picker.php
@@ -0,0 +1,228 @@
+<?php
+/**
+ * Plugin Name: WooCommerce Date Time Picker
+ * Plugin URI: https://github.com/your_github_username/wc-date-time-picker
+ * Description: This plugin adds a date and time picker to WooCommerce products.
+ * Version: 1.0
+ * Author: Elzee Go
+ * Author URI: https://www.elzeego.com
+ * License: GPL2
+ * Text Domain: wc-date-time-picker
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+    exit; // Exit if accessed directly
+}
+
+if ( ! class_exists( 'WC_Date_Time_Picker' ) ) :
+
+class WC_Date_Time_Picker {
+
+    /**
+     * Plugin instance.
+     */
+    protected static $_instance = null;
+
+    /**
+     * Main plugin instance.
+     */
+    public static function instance() {
+        if ( is_null( self::$_instance ) ) {
+            self::$_instance = new self();
+        }
+        return self::$_instance;
+    }
+
+    /**
+     * Constructor.
+     */
+    public function __construct() {
+        add_action( 'woocommerce_before_add_to_cart_button', array( $this, 'add_date_time_picker' ) );
+        add_action( 'woocommerce_add_to_cart_validation', array( $this, 'validate_date_time' ), 10, 3 );
+        add_filter( 'woocommerce_add_cart_item_data', array( $this, 'add_date_time_to_cart_item_data' ), 10, 3 );
+        add_filter( 'woocommerce_get_item_data', array( $this, 'display_date_time_in_cart' ), 10, 2 );
+        add_action( 'woocommerce_checkout_create_order_line_item', array( $this, 'add_date_time_to_order_items' ), 10, 4 );
+
+        // Admin settings
+        add_action( 'admin_menu', array( $this, 'register_taken_dates_times_page' ) );
+        add_filter( 'woocommerce_get_sections_products', array( $this, 'add_settings_section' ) );
+        add_filter( 'woocommerce_get_settings_products', array( $this, 'get_settings' ), 10, 2 );
+    }
+    public function register_taken_dates_times_page() {
+    add_submenu_page( 'woocommerce', 'Taken Dates and Times', 'Taken Dates & Times', 'manage_options', 'wc-taken-dates-times', array( $this, 'taken_dates_times_page' ) );
+}
+
+public function taken_dates_times_page() {
+    global $wpdb;
+
+    $results = $wpdb->get_results( "SELECT order_id, meta_value FROM {$wpdb->prefix}woocommerce_order_itemmeta WHERE meta_key = 'Date & Time'" );
+
+    echo '<div class="wrap"><h1>Taken Dates and Times</h1><table class="wp-list-table widefat fixed striped"><thead><tr><th>Order ID</th><th>Date & Time</th></tr></thead><tbody>';
+
+    foreach ( $results as $row ) {
+    echo '<tr><td>' . esc_html( $row->order_id ) . '</td><td>' . esc_html( $row->meta_value ) . '</td></tr>';
+    }
+
+    echo '</tbody></table></div>';
+}
+
+    public function add_date_time_picker() {
+    wp_enqueue_script('jquery-ui-datepicker');
+    wp_enqueue_style('jquery-ui', '//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css');
+    wp_enqueue_script('jquery-timepicker', '//cdnjs.cloudflare.com/ajax/libs/timepicker/1.3.3.2/jquery.timepicker.min.js', array( 'jquery' ), '1.3.3.2', true );
+    wp_enqueue_style( 'jquery-timepicker', '//cdnjs.cloudflare.com/ajax/libs/timepicker/1.3.3/jquery.timepicker.min.css', array(), '1.3.3' );
+    wp_enqueue_script( 'wc-date-time-picker-script', plugin_dir_url( __FILE__ ) . 'assets/js/wc-date-time-picker.js', array( 'jquery', 'jquery-ui-datepicker', 'jquery-timepicker' ), '1.0', true );
+
+        $style = get_option( 'wc_date_time_picker_style', 'classic' );
+        wp_enqueue_style( 'wc-date-time-picker-style', plugin_dir_url( __FILE__ ) . 'assets/css/' . $style . '.css', array(), '1.0' );
+        wp_enqueue_script( 'wc-date-time-picker-script', plugin_dir_url( __FILE__ ) . 'assets/js/wc-date-time-picker.js', array( 'jquery' ), '1.0', true );
+
+$min_date = get_option( 'wc_date_time_picker_min_date', '0' );
+    $min_time = get_option( 'wc_date_time_picker_min_time', '0' );
+    $available_dates = get_option( 'wc_date_time_picker_available_dates', '' );
+    $available_time_slots = get_option( 'wc_date_time_picker_available_time_slots', '' );
+        $min_date = get_option( 'wc_date_time_picker_min_date', '0' );
+        $min_time = get_option( 'wc_date_time_picker_min_time', '0' );
+        $min_date = get_option( 'wc_date_time_picker_min_date', '0' );
+    $min_time = get_option( 'wc_date_time_picker_min_time', '0' );
+    $available_dates = get_option( 'wc_date_time_picker_available_dates', '' );
+    $available_time_slots = get_option( 'wc_date_time_picker_available_time_slots', '' );
+
+    $available_dates = array_map( 'trim', explode( ',', $available_dates ) );
+$available_time_slots = array_map( 'trim', explode( ',', $available_time_slots ) );
+wp_localize_script( 'wc-date-time-picker-script', 'wcDateTimePickerData', array(
+    'min_date' => $min_date,
+    'min_time' => $min_time,
+    'available_dates' => $available_dates,
+    'available_time_slots' => $available_time_slots,
+));
+
+
+        ?>
+        <div class="wc-date-time-picker">
+ <!-- Date picker and time picker input elements -->
+            <p>
+                <label for="wc-date-picker"><?php _e( 'Pasirinkite datą', 'wc-date-time-picker' ); ?></label>
+                <input type="text" id="wc-date-picker" name="wc_date" data-min-date="<?php echo esc_attr( $min_date ); ?>">
+            </p>
+            <p>
+                <label for="wc-time-picker"><?php _e( 'Pasirinkite', 'wc-date-time-picker' ); ?></label>
+                <input type="text" id="wc-time-picker" name="wc_time" data-min-time="<?php echo esc_attr( $min_time ); ?>">
+            </p>
+        </div>
+        <script>
+        jQuery(document).ready(function($) {
+            var availableDates = <?php echo json_encode( $available_dates ); ?>;
+            var availableTimeSlots = <?php echo json_encode( $available_time_slots ); ?>;
+            // ...
+        });
+    </script>
+
+    <?php
+}
+
+    public function validate_date_time( $passed, $product_id, $quantity ) {
+        if ( empty( $_POST['wc_date'] ) || empty( $_POST['wc_time'] ) ) {
+            $passed = false;
+            wc_add_notice( __( 'Please choose a date and time before adding
+            to the cart.', 'wc-date-time-picker' ), 'error' );
+        }
+
+        return $passed;
+    }
+
+    public function add_date_time_to_cart_item_data( $cart_item_data, $product_id, $variation_id ) {
+        if ( isset( $_POST['wc_date'] ) && isset( $_POST['wc_time'] ) ) {
+            $cart_item_data['wc_date_time'] = sanitize_text_field( $_POST['wc_date'] ) . ' ' . sanitize_text_field( $_POST['wc_time'] );
+        }
+
+        return $cart_item_data;
+    }
+
+    public function display_date_time_in_cart( $item_data, $cart_item ) {
+        if ( isset( $cart_item['wc_date_time'] ) ) {
+            $item_data[] = array(
+                'key'     => __( 'Date & Time', 'wc-date-time-picker' ),
+                'value'   => wc_clean( $cart_item['wc_date_time'] ),
+                'display' => '',
+            );
+        }
+
+        return $item_data;
+    }
+
+    public function add_date_time_to_order_items( $item, $cart_item_key, $values, $order ) {
+        if ( isset( $values['wc_date_time'] ) ) {
+            $item->add_meta_data( __( 'Date & Time', 'wc-date-time-picker' ), $values['wc_date_time'], true );
+        }
+    }
+
+    //
+    // Admin settings
+    public function add_settings_section( $sections ) {
+        $sections['wc_date_time_picker'] = __( 'Date Time Picker', 'wc-date-time-picker' );
+        return $sections;
+    }
+
+    public function get_settings( $settings, $current_section ) {
+        if ( $current_section == 'wc_date_time_picker' ) {
+            $settings = array(
+                array(
+                    'title' => __( 'Date Time Picker Settings', 'wc-date-time-picker' ),
+                    'type'  => 'title',
+                    'desc'  => '',
+                    'id'    => 'wc_date_time_picker_options',
+                ),
+                array(
+                    'title'   => __( 'Style', 'wc-date-time-picker' ),
+                    'desc'    => __( 'Choose the style for the date time picker.', 'wc-date-time-picker' ),
+                    'id'      => 'wc_date_time_picker_style',
+                    'default' => 'classic',
+                    'type'    => 'select',
+                    'options' => array(
+                        'classic' => __( 'Classic', 'wc-date-time-picker' ),
+                        'modern'  => __( 'Modern', 'wc-date-time-picker' ),
+                    ),
+                ),
+                array(
+                    'title'   => __( 'Minimum Date', 'wc-date-time-picker' ),
+                    'desc'    => __( 'Set the minimum selectable date (in days from now). Default: 0', 'wc-date'),
+                    'type'    => 'number',
+                    'id'      => 'wc_date_time_picker_min_date',
+                    'default' => '0',
+                ),
+                array(
+                    'title'   => __( 'Minimum Time', 'wc-date-time-picker' ),
+                    'desc'    => __( 'Set the minimum selectable time (in hours from now). Default: 0', 'wc-date-time-picker' ),
+                    'type'    => 'number',
+                    'id'      => 'wc_date_time_picker_min_time',
+                    'default' => '0',
+                ),
+                array(
+                    'type' => 'sectionend',
+                    'id'   => 'wc_date_time_picker_options',
+                ),
+                array(
+                    'title'   => __( 'Available Dates', 'wc-date-time-picker' ),
+                    'desc'    => __( 'Set the available dates in the format YYYY-MM-DD, separated by commas.', 'wc-date-time-picker' ),
+                     'id'      => 'wc_date_time_picker_available_dates',
+                     'default' => '',
+                     'type'    => 'textarea',
+                ),
+                array(
+                    'title'   => __( 'Available Time Slots', 'wc-date-time-picker' ),
+                     'desc'    => __( 'Set the available time slots in the format HH:mm, separated by commas.', 'wc-date-time-picker' ),
+                     'id'      => 'wc_date_time_picker_available_time_slots',
+                     'default' => '',
+                ),
+            );
+        }
+
+        return $settings;
+    }
+
+}
+
+WC_Date_Time_Picker::instance();
+
+endif;
