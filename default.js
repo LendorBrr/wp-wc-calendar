79c5e1833d226dfdfc62ff6dc96061758ab3426e
LendorBrr <96260311+LendorBrr@users.noreply.github.com>
2023-03-26 22:03:17 +0300

    Update default.js

--- default.js
+++ default.js
@@ -1,56 +1,51 @@
 jQuery(document).ready(function($) {
-  if ($(".wc-date-time-picker").length > 0) {
-    var product_id = wc_date_time_picker_vars.product_id;
-    var allowed_products = wc_date_time_picker_vars.allowed_products;
+  // Initialize date and time pickers
+  let minDate = parseInt($("#wc-date-picker").data("min-date"), 10);
+  let minTime = parseInt($("#wc-time-picker").data("min-time"), 10);
+  var availableDates = wcDateTimePickerData.available_dates;
+  var availableTimeSlots = wcDateTimePickerData.available_time_slots;
+  var allowed_products = JSON.parse(wc_datetime_picker_params.allowed_products); // Parse the JSON string
+  var current_product_id = parseInt(wc_datetime_picker_params.current_product_id, 10);
 
-    if (allowed_products.includes(product_id)) {
-      $(".wc-date-time-picker").each(function() {
-        var $dateTimePicker = $(this);
+  if (allowed_products.includes(current_product_id)) {
 
-        // Initialize datepicker
-        $dateTimePicker.find(".wc-date-picker").datepicker({
-          dateFormat: "yy-mm-dd",
-          minDate: 0
-        });
-
-        // Initialize timepicker
-        $dateTimePicker.find(".wc-time-picker").slider({
-          range: "min",
-          min: 0,
-          max: 1440,
-          step: 15,
-          slide: function(event, ui) {
-            var minutes = ui.value % 60;
-            var hours = (ui.value - minutes) / 60;
-            $dateTimePicker.find(".wc-time-picker-display").text(("0" + hours).slice(-2) + ":" + ("0" + minutes).slice(-2));
-          }
-        });
-
-        // Set default time value
-        $dateTimePicker.find(".wc-time-picker-display").text("00:00");
-      });
-
-      $("form.cart").on("submit", function(e) {
-        var $form = $(this);
-        var $dateTimePicker = $form.find(".wc-date-time-picker");
-        if ($dateTimePicker.length > 0) {
-          var date = $dateTimePicker.find(".wc-date-picker").val();
-          var time = $dateTimePicker.find(".wc-time-picker-display").text();
-
-          if (!date || !time) {
-            e.preventDefault();
-            alert("Please select a date and time before adding to the cart.");
-            return false;
-          }
-
-          $("<input>")
-            .attr("type", "hidden")
-            .attr("name", "wc_date_time")
-            .val(date + " " + time)
-            .appendTo($form);
-        }
-      });
-    }
+  function isAvailableDate(date) {
+    var dateString = jQuery.datepicker.formatDate('yy-mm-dd', date);
+    return (availableDates.indexOf(dateString) > -1);
   }
-});
 
+  $("#wc-date-picker").datepicker({
+    minDate: minDate,
+    dateFormat: "yy-mm-dd",
+    beforeShowDay: function(date) {
+      if (availableDates.length > 0) {
+        return [isAvailableDate(date)];
+      }
+      return [true];
+    },
+  });
+
+  $("#wc-time-picker").timepicker({
+    minTime: minTime,
+    timeFormat: "H:i",
+    disableTimeRanges: availableTimeSlots.length > 0 ? [] : [['12am', '11:59pm']],
+  });
+
+  if (availableTimeSlots.length > 0) {
+    $('#wc-time-picker').on('show.timepicker', function() {
+      var instance = $(this).data('timepicker');
+      var date = $('#wc-date-picker').datepicker('getDate');
+
+      if (isAvailableDate(date)) {
+        instance.option('disableTimeRanges', []);
+        availableTimeSlots.forEach(function(timeSlot) {
+          var timeRange = timeSlot.split('-');
+          instance.option('disableTimeRanges').push(timeRange);
+        });
+      } else {
+        instance.option('disableTimeRanges', [['12am', '11:59pm']]);
+      }
+    });
+  }
+  }
+};
